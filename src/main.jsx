3714ab7da5b1c476a648a3dcbff3bb5f20454df0
Derian Molina <derianmolina220@gmail.com>
2023-08-27 17:25:03 -0700

    carpetas cambiadas

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Pantalla } from './ComponentesCalculadora/elementosinternos'
+import { Pantalla } from './elementosinternos'
 import './global.css'
 //import { ContenedorCalculadora} from './claculadora'
 import { Father } from './ComponentesAnidados/father'
